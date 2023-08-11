6106f20b360233b4db4b9135305745968eeb21a4
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-08-11 09:12:45 +0000

    importing add file

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+if __chimezie__ == "__main__":
+    """Print the sum of two digits."""
+    from add_0 import add
+
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
