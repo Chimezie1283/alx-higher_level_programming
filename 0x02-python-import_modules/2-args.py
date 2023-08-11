b24cf5fd199e71f5c5327be3fdad0de280f0c7d7
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-08-11 09:28:16 +0000

    prints two argumentz

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,13 +4,13 @@ if __name__ == "__main__":
     """Print the number and list of arguments."""
     import sys
 
-    countar = len(sys.argv) - 1
-    if countar == 0:
+    count = len(sys.argv) - 1
+    if count == 0:
         print("0 arguments.")
-    elif countar == 1:
+    elif count == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(countar))
-    for i in range(countar):
+        print("{} arguments:".format(count))
+    for i in range(count):
         print("{}: {}".format(i + 1, sys.argv[i + 1]))
 
