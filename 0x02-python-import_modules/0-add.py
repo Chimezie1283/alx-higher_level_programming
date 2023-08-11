f2dea5b5d7f403be9336702be49ff0b9b89773ce
chimezie1283 <agbo.chimezie1283@gmail.com>
2023-08-11 14:00:27 +0000

    add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    """Print the sum of two digits."""
-    from add_0 import add
+""" prints the sum of 1 and two """
 
+from add_0 import add
+
+if __name__ == "__main__":
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
